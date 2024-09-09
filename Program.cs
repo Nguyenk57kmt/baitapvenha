6a239c4cc4e5cabf152144d5c0fb6ffcb58552d3
Nguyenk57kmt <k215480106115@tnut.edu.vn>
2024-09-09 16:27:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mylibrary;
+
+namespace bài_tập
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so thu nhat:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Nhap so thu hai:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            Calculator calculator = new Calculator();
+            int sigle  = calculator.Add(x, y);
+
+
+            Console.WriteLine("hieu cua hai so la: " + sigle);
+
+
+            Console.ReadKey();
+        }
+    }
+}
